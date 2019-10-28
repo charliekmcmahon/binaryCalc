2e48bb738a05d91cf54804d6c506a93c40ace405
Charlie McMahon <charliekmcmahon@gmail.com>
2019-10-28 12:15:12 +1000

    Initial Commit

---
+++ binaryCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  binaryCalc
+//
+//  Created by Charlie McMahon on 28/10/19.
+//  Copyright © 2019 Charlie McMahon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
