b64a85ef99825015ea7aa2a62fe9ccc6e3237d3f
Charlie McMahon <charliekmcmahon@gmail.com>
2019-10-30 11:27:27 +1000

    updated

--- binaryCalc/ViewController.swift
+++ binaryCalc/ViewController.swift
@@ -49,6 +49,8 @@ class ViewController: UIViewController {
             binaryVal = String(one) + String(two) + String(three) + String(four) + String(five) + String(six) + String(seven) + String(eight)
         
         binaryLabel.text = binaryVal
+        let number = String(decoding: binaryVal, as: UTF8.self)
+        bigCharLabel.text = String(number)
         
         }
     
@@ -144,8 +146,7 @@ class ViewController: UIViewController {
         binaryLabel.text = binaryVal
     }
     
-    // TEST THIS!!!
-    let number = Int(binaryVal, radix: 2)
+    // 
     
     
     
